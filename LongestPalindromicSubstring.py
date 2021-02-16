b8a1c9a76f11dcbb13275ecb08ad6971db24ba5b
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-16 17:30:12 +0530

    Update LongestPalindromicSubstring.py

--- LongestPalindromicSubstring.py
+++ LongestPalindromicSubstring.py
@@ -24,3 +24,33 @@ class Solution:
                         l = len(w)
         return p
         
+	
+	
+#CODENO.2: work well with no err like Time exceed err
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        longest_str = ''
+        n = [len(s) - 1]
+
+        for diff in range(1, len(s)):
+            n.append(n[0] + diff)
+            n.append(n[0] - diff)
+
+        for i in n:
+            if (min(i + 1, 2 * len(s) - 1 - i) <= len(longest_str)):
+                break
+
+            if i % 2 == 0:
+                left, right = (i // 2) - 1, (i // 2) + 1
+            else:
+                left, right = i // 2, (i // 2) + 1
+
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+
+            if right - left - 1 > len(longest_str):
+                longest_str = s[left + 1: right]
+
+        return longest_str
+	
