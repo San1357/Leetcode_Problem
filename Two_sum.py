e03bc7fe172730abbca53c880ce661709e99a1d8
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-18 12:29:41 +0530

    Create Two_sum.py

---
+++ Two_sum.py
@@ -0,0 +1,18 @@
+#Problem:Two Sum 
+
+
+
+#CODE:
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+
+        for index, num in enumerate(nums):
+            other = target - num 
+
+            if other in seen:
+                return[seen[other],index]
+            else:
+                seen[num] = index
+        return []
