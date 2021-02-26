61dd76e67007851beb2d4cb1928fd2b17ef88d9d
San Ayush <41052723+San1357@users.noreply.github.com>
2021-02-26 12:45:18 +0530

    Create Jump_Game.py

---
+++ Jump_Game.py
@@ -0,0 +1,18 @@
+'''Problem : JUMP GAME '''
+
+#CODE :
+
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        jump = nums[0]
+        for i in range(1,len(nums)):
+            if jump == 0:
+                return False
+            jump -= 1
+            jump = max(jump,nums[i])
+        return True
+            
