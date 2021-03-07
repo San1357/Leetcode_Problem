49a844b2cae349d594757b64de334d1097d79939
San Ayush <41052723+San1357@users.noreply.github.com>
2021-03-07 13:27:29 +0530

    Create Partition_List.py

---
+++ Partition_List.py
@@ -0,0 +1,19 @@
+'''Problem : Partition List '''
+
+#CODE :
+
+ def partition(self, head: ListNode, x: int) -> ListNode:
+        if not head or not head.next: return head
+        dummy_head = prv = ListNode("inf")
+        prv.next = head
+        while head and head.next:
+            if head.val >= x and head.next.val<x:
+                temp = head.next.next
+                head.next.next = prv.next
+                prv.next = head.next
+                head.next = temp
+            else:
+                head = head.next
+            if prv.next.val < x:
+                prv=prv.next
+        return dummy_head.next
